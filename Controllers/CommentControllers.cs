e85d57f4b2b21e82e5b9261a97410a88cb565c92
jamesconfy <bobdence@gmail.com>
2024-10-24 11:30:59 +0100

    Switched to my account

---
+++ Controllers/CommentControllers.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockMarket.Internal.Data;
+
+namespace StockMarket.Controllers
+{
+    [Route("/api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public StockController(ApplicationDBContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var stocks = _dbContext.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id) 
+        {
+            var stock = _dbContext.Stocks.Find(id);
+
+            if (stock == null) { 
+                return NotFound(stock);
+            }
+
+            return Ok(stock);
+        }
+  
+    }
+}
---
+++ Internal/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using StockMarket.Internal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace StockMarket.Internal.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
