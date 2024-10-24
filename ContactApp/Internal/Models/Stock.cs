9a75190eb6ad2bf7d90f7fbeb07ccbf5b92f86ca
mosesonuh <ichadomoses@gmail.com>
2024-10-24 11:20:35 +0100

    Add project files.

---
+++ ContactApp/Internal/Models/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockMarket.Internal.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; } = 0;
+
+        public string CompanyName { get; set; } = string.Empty;
+        public string Symbol { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; } = 0;
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
